6250e71085c7b381edf3d44a23a18e43ce837877
Merrin <merrin@rabidtech.co.nz>
2013-12-15 06:48:00 +1300

    did some more hugs

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ $("#response").keyup(function(event){
         var response = $('#response').val();
         $("#history").append('<div class="human-response"> >' + response + '</div>')
         $('#response').val("");
+        $("#history").append('<div class="computer-response">' + "I don't know what '" + response + "' means. I hope it isn't anything rude." + '</div>')
     }
 });
+
+//score game based on number of hugs
+//let people steal a bike but take a point off. and don't give the point back if they give the bike back: "That won't ASSUAGE your CONSCIENCE."
