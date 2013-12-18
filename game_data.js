b5466af716acb7d767d9338882d51afb64009c99
Merrin <merrin@rabidtech.co.nz>
2013-12-18 23:02:20 +1300

    yes verbs

--- game_data.js
+++ game_data.js
@@ -1,29 +1,20 @@
-function Cmd(functor,cmds,condition) {
-        functor.commands = cmds;
-        functor.condition = condition;
-        return functor;
+function Go(dir, destination) {
+  return dir;
 }
 
-function Go(dir,key,condition) {
-        var mapping = {
-                east:"left",
-                west:"right",
-        };
-        var commands = ["go "+dir,"exit "+dir,dir];
-        if(dir in mapping)
-                commands = commands.concat(["go "+mapping[dir],"exit "+mapping[dir]]);
-        var go = Cmd(function() { go_to(key); },commands,condition);
-        go.go = key; // so npcs know its navigable
-        go.dir = dir; // for "exits" help
-        return go;
-}
 
 var locations = {
   outside: {
     name:"Outside Enspiral",
     description:"You are outside Enspiral Space.",
+    directions: {
+      north: "stairs",
+      south: "road",
+      west: "bar",
+      east: "pandoro"
+    },
     commands:[
-      Go("north", "stairs")
+      Go("left", "stairs")
     ]
   },
   stairs: {
@@ -38,156 +29,8 @@ var locations = {
     name:"Enspiral entrance",
     description:"The sound of the coffee grinder doesn't quite drown out the excited scheming taking place in the kitchen. There are bikes here.",
     commands:[
-    Go("north", "enspiral_space"),
     Go("south", "stairs"),
-    Go("west", "toilets")
     ]
-  },
-  toilets: {
-    name:"Toilets",
-    description:"These are toilets. I don't know what you expected. You don't really need to go anyway. You are just standing around awkwardly. It is weird.",
-    commands:[
-    Go("east", "entrance")
-    ]
-  },
-  enspiral_space: {
-    name:"Inside Enspiral Space",
-    description:"You have entered Enspiral Space.",
-    commands:[
-    Go("east", "desks"),
-    Go("south", "entrance"),
-    Go("west", "boardroom")
-    ]
-  },
-  desks: {
-    name:"First desks",
-    description:"You can see some HIPPIES. To your NORTH is RABID.",
-    commands:[
-    Go("north", "rabid"),
-    Go("west", "enspiral_space")
-    ]
-  },
-  rabid: {
-    name:"Rabid land",
-    description:"The den of iniquity.",
-    commands:[
-    Go("south", "desks")
-    ]
-  }
-}
-
-var npcs = {
-  josh: {
-    name: "Josh Forde",
-    lines:{
-      encounter: [
-        "Josh sees you coming from the corner of his eye and his expression of intense focus turns into a wide grin. He stands up from his chair to face you. \"It's great of you to visit!\" he exclaims. He looks like he wants a HUG."
-      ],
-      greet: [
-        "Josh smiles widely at you.",
-      ],
-      hug: [
-        "You hug Josh. He embraces you like a sibling and pats you on the back. \"I'm just so pleased to see you,\" he says.",
-      ],
-    }
-  },
-  rob: {
-    name: "Rob Ramsay",
-    lines:{
-      encounter: [
-        "Rob looks up from his computer and sees you. He raises one eyebrow, smiles, and nods knowingly. He removes his headphones, which are pumping house music, to greet you."
-      ],
-      greet: [
-        "Rob leans in conspiratorially. \"Do you know anything about chemistry?\" he asks.",
-      ],
-      hug: [
-        "You hug Rob. A solid embrace.",
-      ],
-    }
-  },
-  breccan: {
-    name: "Breccan McLeod",
-    lines:{
-      encounter: [
-        "Breccan sees you coming, smiles, and swivels his chair around to greet you. You meet his eyes and he smiles. \"Greetings,\" he says." 
-      ],
-      greet: [
-        "Breccan smiles at you. \"How have you been?\" he asks. As you answer, he nods, and asks follow-up questions. You get the feeling he's really glad you visited.",
-      ],
-      hug: [
-        "As you hug Breccan you note to yourself how nice his suit feels.",
-      ],
-    }
-  },
-  merrin: {
-    name: "Merrin Macleod",
-    lines:{
-      encounter: [
-        "Merrin is staring intently at the computer nestled in the chaos atop her desk. She hears your approach and turns to face you; her face lights up when she sees it's you. She waves." 
-      ],
-      greet: [
-        "Merrin grins and waves at you.",
-      ],
-      hug: [
-        "You hug Merrin. \"Thank you so much for visiting,\" she says.",
-      ],
-    }
-  },
-  eoin: {
-    name: "Eoin Kelly",
-    lines:{
-      encounter: [
-        "" 
-      ],
-      greet: [
-        "",
-      ],
-      hug: [
-        "",
-      ],
-    }
-  },
-  matt: {
-    name: "Mathew Hartley",
-    lines:{
-      encounter: [
-        "" 
-      ],
-      greet: [
-        "",
-      ],
-      hug: [
-        "",
-      ],
-    }
-  },
-  lachlan: {
-    name: "Lachlan Priest",
-    lines:{
-      encounter: [
-        "" 
-      ],
-      greet: [
-        "",
-      ],
-      hug: [
-        "",
-      ],
-    }
-  },
-  megan: {
-    name: "Megan Bowra-Dean",
-    lines:{
-      encounter: [
-        "" 
-      ],
-      greet: [
-        "",
-      ],
-      hug: [
-        "",
-      ],
-    }
   },
 }
 
--- script.js
+++ script.js
@@ -12,26 +12,90 @@
 //npc
 var score = 0;
 var turn_count = 0;
+var current_location = locations.outside;
 
 function computer_print(response) {
 	$("#history").append('<div class="computer-response">' + response + '</div>')
 }
 
-//turn
+function Walk (direction) {
+	var directions = ["north", "south", "east", "west"];
+	if(directions.indexOf(direction) > -1) {
+		return ("walking " + direction)
+	}
+	else {
+		return ("invalid direction")
+	}
+}
+
+function Hug (recipient) {
+	return ("hugging " + recipient)
+}
+
+function Take (object) {
+	return ("taking " + object)
+}
+
+function Kill (victim) {
+	return ("killing " + victim)
+}
+
+function check_for_verbs(response) {
+	if (response.substring(0,2) == "go") {
+    	return Walk(response.substr(3));
+	}
+	if (response.substring(0,4) == "walk") {
+    	return Walk(response.substr(5));
+	}
+	if (response.substring(0,3) == "hug") {
+		return Hug(response.substr(4));
+	}
+	if (response.substring(0,7) == "embrace") {
+		return Hug(response.substr(8));
+	}
+	if (response.substring(0,4) == "take") {
+		return Take(response.substr(5));
+	}
+	if (response.substring(0,3) == "get") {
+		return Take(response.substr(4));
+	}
+	if (response.substring(0,7) == "pick up") {
+		return Take(response.substr(8));
+	}
+	if (response.substring(0,4) == "kill") {
+		return Kill(response.substr(5));
+	}
+	if (response.substring(0,6) == "murder") {
+		return Kill(response.substr(7));
+	}
+	else {
+		return false;
+	}
+}
+
 
 function turn(response) {
     $("#history").append('<div class="human-response"> >' + response + '</div>') //reprint response
     $('#response').val(""); //clear response box
     turn_count ++;
-    if(response === "LEFT") {
-	    computer_print("You are a butt");
+    var check_response = current_location.commands.indexOf(response);
+    if(check_response > -1) {
+    	computer_print("You head " + current_location.commands[check_response] + " to " + current_location.name + ".")
+	    current_location = locations.stairs;
+	    computer_print(current_location.description);
 	    score ++;
     }
     else {
-	    computer_print("I don't know what '" + response + "' means. I hope it isn't anything rude.")
+	    if(check_for_verbs(response) === false){
+		    computer_print("I don't know what '" + response + "' means. I hope it isn't anything rude.")
+	    }
+	    else {
+	    	computer_print(check_for_verbs(response));
+	    }
     }
     computer_print("Your score is " + score + ".");
     computer_print("This is turn " + turn_count + ".")
+    computer_print("You are at " + current_location.name);
 }
 
 //score
